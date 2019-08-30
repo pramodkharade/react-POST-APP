7e4128a513eb3cfd7c88b6e14a6b38a4c4433025
pramodkharade <kharade.pramod91@gmail.com>
2019-08-30 10:18:09 +0530

    to get listen socket emit post event from server

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -35,9 +35,26 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
-    openSocket('http://localhost:8080');
+   const socket =  openSocket('http://localhost:8080');
+   socket.on('posts',data=>{
+      if(data.action==='create'){
+        this.addPost(data.post);
+      }
+   });
   }
-
+addPost = post=>{
+  this.setState(prevState=>{
+    const updatedPosts = [...prevState.posts];
+    if(prevState.postPage===1){
+      updatedPosts.pop();
+      updatedPosts.unshift(post);
+    }
+    return {
+      posts: updatedPosts,
+      totalPosts : prevState.totalPosts + 1
+    };
+  });
+};
   loadPosts = direction => {
     if (direction) {
       this.setState({ postsLoading: true, posts: [] });
@@ -153,9 +170,7 @@ class Feed extends Component {
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
-          } else if (prevState.posts.length < 2) {
-            updatedPosts = prevState.posts.concat(post);
-          }
+          } 
           return {
             posts: updatedPosts,
             isEditing: false,
